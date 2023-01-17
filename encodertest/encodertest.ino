dccc3eb5b30a6c8311861d32fa690a8b88fd21b8
Fernando Quisaguano <81866283+FerJeffQ@users.noreply.github.com>
2023-01-17 12:03:36 -0500

    prueba_encoders

---
+++ encodertest/encodertest.ino
@@ -0,0 +1,148 @@
+#define encoder0PinA 3      // encoder 1
+#define encoder0PinB 2
+
+#define encoder1PinA 21     // encoder 2
+#define encoder1PinB 20
+
+unsigned long currentMillis;
+unsigned long previousArmMillis;
+unsigned long previousMillis;
+
+volatile long encoder0Pos = 0;    // encoder 1
+volatile long encoder1Pos = 0;    // encoder 2
+
+
+void setup() {
+  Serial.begin(57600);
+  pinMode(encoder0PinA, INPUT_PULLUP);    // encoder pins
+  pinMode(encoder0PinB, INPUT_PULLUP);
+
+  pinMode(encoder1PinA, INPUT_PULLUP); 
+  pinMode(encoder1PinB, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(encoder0PinA), doEncoderA, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encoder0PinB), doEncoderB, CHANGE); 
+
+  attachInterrupt(digitalPinToInterrupt(encoder1PinA), doEncoderC, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encoder1PinB), doEncoderD, CHANGE); 
+
+}
+
+
+void loop() {
+  currentMillis = millis(); 
+  if (currentMillis - previousMillis >= 10) {  // start timed loop for everything else
+         previousMillis = currentMillis;
+         Serial.print(encoder0Pos);
+         Serial.print(",");
+         Serial.println(encoder1Pos);
+  }
+
+}
+
+// ************** encoders interrupts **************
+
+// ************** encoder 1 *********************
+
+
+void doEncoderA(){  
+
+  /
+  if (digitalRead(encoder0PinA) == HIGH) { 
+    // check channel B to see which way encoder is turning
+    if (digitalRead(encoder0PinB) == LOW) {  
+      encoder0Pos = encoder0Pos + 1;         // CW
+    } 
+    else {
+      encoder0Pos = encoder0Pos - 1;         // CCW
+    }
+  }
+  else   // must be a high-to-low edge on channel A                                       
+  { 
+    // check channel B to see which way encoder is turning  
+    if (digitalRead(encoder0PinB) == HIGH) {   
+      encoder0Pos = encoder0Pos + 1;          // CW
+    } 
+    else {
+      encoder0Pos = encoder0Pos - 1;          // CCW
+    }
+  }
+ 
+}
+
+void doEncoderB(){  
+
+  if (digitalRead(encoder0PinB) == HIGH) {   
+   // check channel A to see which way encoder is turning
+    if (digitalRead(encoder0PinA) == HIGH) {  
+      encoder0Pos = encoder0Pos + 1;         // CW
+    } 
+    else {
+      encoder0Pos = encoder0Pos - 1;         // CCW
+    }
+  }
+  // Look for a high-to-low on channel B
+  else { 
+    // check channel B to see which way encoder is turning  
+    if (digitalRead(encoder0PinA) == LOW) {   
+      encoder0Pos = encoder0Pos + 1;          // CW
+    } 
+    else {
+      encoder0Pos = encoder0Pos - 1;          // CCW
+    }
+  }
+  
+
+}
+
+// ************** encoder 2 *********************
+
+void doEncoderC(){  
+
+
+  if (digitalRead(encoder1PinA) == HIGH) { 
+    
+    if (digitalRead(encoder1PinB) == LOW) {  
+      encoder1Pos = encoder1Pos - 1;         // CW
+    } 
+    else {
+      encoder1Pos = encoder1Pos + 1;         // CCW
+    }
+  }
+  else                                        
+  { 
+   
+    if (digitalRead(encoder1PinB) == HIGH) {   
+      encoder1Pos = encoder1Pos - 1;          // CW
+    } 
+    else {
+      encoder1Pos = encoder1Pos + 1;          // CCW
+    }
+  }
+ 
+}
+
+void doEncoderD(){  
+
+  // look for a low-to-high on channel B
+  if (digitalRead(encoder1PinB) == HIGH) {   
+   
+    if (digitalRead(encoder1PinA) == HIGH) {  
+      encoder1Pos = encoder1Pos - 1;         // CW
+    } 
+    else {
+      encoder1Pos = encoder1Pos + 1;         // CCW
+    }
+  }
+
+  else { 
+    
+    if (digitalRead(encoder1PinA) == LOW) {   
+      encoder1Pos = encoder1Pos - 1;          // CW
+    } 
+    else {
+      encoder1Pos = encoder1Pos + 1;          // CCW
+    }
+  }
+  
+
+}
